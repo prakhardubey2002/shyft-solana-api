d6cd4593dd6c5441f6bd12a207ce5d3cc736e6eb
Pintu Das <rex@workwithme.in>
2022-06-01 19:05:42 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
