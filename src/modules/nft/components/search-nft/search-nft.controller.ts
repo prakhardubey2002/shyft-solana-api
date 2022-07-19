5e69b92811ed2fdb6d1242238cee6d2f145ffe6b
vgvishesh <vgvishesh2008@gmail.com>
2022-07-20 00:33:33 +0530

    correcting search api monitoring

--- src/modules/nft/components/search-nft/search-nft.controller.ts
+++ src/modules/nft/components/search-nft/search-nft.controller.ts
@@ -2,13 +2,16 @@ import { ApiTags, ApiSecurity } from '@nestjs/swagger';
 import { Controller, Get, Query, Req, Version } from '@nestjs/common';
 import { SearchNftService } from './search-nft.service';
 import { SearchAttributesOpenApi } from './open-api';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { ApiInvokeEvent } from 'src/modules/api-monitor/api.event';
 
 @ApiTags('NFT')
 @ApiSecurity('api_key', ['x-api-key'])
 @Controller('nft')
 export class SearchNftcontroller {
 	constructor(
-		private searchNftService: SearchNftService
+		private searchNftService: SearchNftService,
+		private eventEmitter: EventEmitter2
 	) { }
 
 	@SearchAttributesOpenApi()
@@ -19,6 +22,10 @@ export class SearchNftcontroller {
 		@Req() request: any
 	): Promise<any> {
 		const result = await this.searchNftService.searchNftsByAttributes(query, request.id)
+
+		const nftCreationEvent = new ApiInvokeEvent('nft.search', request.apiKey);
+		this.eventEmitter.emit('api.invoked', nftCreationEvent);
+
 		return {
 			success: true,
 			message: "filtered NFTs",
