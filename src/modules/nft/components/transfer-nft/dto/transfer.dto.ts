e832fd7bc7b59258b88130e500a5c441ace22eef
Pintu Das <rex@workwithme.in>
2022-08-09 18:50:38 +0530

    need to fix: transfer NFT without private_key

--- src/modules/nft/components/transfer-nft/dto/transfer.dto.ts
+++ src/modules/nft/components/transfer-nft/dto/transfer.dto.ts
@@ -56,3 +56,57 @@ export class TransferNftDto {
   })
   readonly transfer_authority: string;
 }
+
+export class TransferNftDetachDto {
+  @ApiProperty({
+    title: 'network',
+    type: String,
+    enum: WalletAdapterNetwork,
+    description: 'Select solana blockchain environment ',
+  })
+  @IsNotEmpty()
+  readonly network: WalletAdapterNetwork;
+
+  @ApiProperty({
+    title: 'Address of the NFT',
+    type: String,
+    description: 'address of the NFT to be transfered',
+    example: 'HJ32KZye152eCFQYrKDcoyyq77dVDpa8SXE6v8T1HkBP',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly token_address: string;
+
+  @ApiProperty({
+    title: 'Address of wallet containing the NFT',
+    type: String,
+    description: 'Address of the wallet containing the NFT to be transfered',
+    example: '2fmz8SuNVyxEP6QwKQs6LNaT2ATszySPEJdhUDesxktc',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly from_address: string;
+
+  @ApiProperty({
+    title: 'To Wallet Address',
+    type: String,
+    description: 'Address of the wallet to which the NFT will be transfered',
+    example: 'BFefyp7jNF5Xq2A4JDLLFFGpxLq5oPEFKBAQ46KJHW2R',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly to_address: string;
+
+  @ApiProperty({
+    title: 'Transfer Authority',
+    type: Boolean,
+    description: 'Transfer update authority to the receiver or not, default TRUE',
+    example: 'true',
+  })
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => {
+    return value ?? true;
+  })
+  readonly transfer_authority: string;
+}
--- src/modules/nft/components/transfer-nft/transfer-nft.service.ts
+++ src/modules/nft/components/transfer-nft/transfer-nft.service.ts
@@ -4,91 +4,196 @@ import { Connection, NodeWallet } from '@metaplex/js';
 import { AccountUtils } from 'src/common/utils/account-utils';
 import { TransferNftDto } from './dto/transfer.dto';
 import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
   createTransferCheckedInstruction,
+  getAssociatedTokenAddress,
   getOrCreateAssociatedTokenAccount,
+  TOKEN_PROGRAM_ID,
 } from '@solana/spl-token';
 import { UpdateNftService } from '../update-nft/update-nft.service';
 import { Metaplex } from '@metaplex-foundation/js';
+import { createUpdateMetadataAccountV2Instruction, DataV2 } from '@metaplex-foundation/mpl-token-metadata';
+import { Metadata } from '@metaplex-foundation/mpl-token-metadata-depricated';
 
 @Injectable()
 export class TransferNftService {
   constructor(private updateNftService: UpdateNftService) {}
 
   async transferNft(transferNftDto: TransferNftDto): Promise<any> {
-    const {
-      network,
-      from_address: fromAdress,
-      transfer_authority,
-    } = transferNftDto;
-    const tokenAddress = new PublicKey(transferNftDto.token_address);
-    const toAddress = new PublicKey(transferNftDto.to_address);
-
-    const connection = new Connection(clusterApiUrl(network), 'confirmed');
-
-    //generate wallet
-    const fromKeypair = AccountUtils.getKeypair(fromAdress);
-    const wallet = new NodeWallet(fromKeypair);
-
-    //Find token accounts
-    const fromAccount = await getOrCreateAssociatedTokenAccount(
-      connection,
-      fromKeypair,
-      tokenAddress,
-      fromKeypair.publicKey,
-    );
-    const toAccount = await getOrCreateAssociatedTokenAccount(
-      connection,
-      fromKeypair,
-      tokenAddress,
-      toAddress,
-    );
-
-    //Create transfer instruction
-    const ix = createTransferCheckedInstruction(
-      fromAccount.address,
-      tokenAddress,
-      toAccount.address,
-      fromKeypair.publicKey,
-      1,
-      0,
-    );
-    const tx = new Transaction().add(ix);
-    tx.feePayer = fromKeypair.publicKey;
-    tx.recentBlockhash = (
-      await connection.getLatestBlockhash('finalized')
-    ).blockhash;
-
-    //Sign and send it
-    const signedTx = await wallet.signTransaction(tx);
-    const transferTxId = await connection.sendRawTransaction(
-      signedTx.serialize(),
-    );
-
-    const isSuccessful = await connection.confirmTransaction(transferTxId);
-
-    //Now update the authority
-    let updateTxId;
-    if (isSuccessful.value.err === null && transfer_authority) {
-      //Update Authority
-      const metaplex = Metaplex.make(connection);
-      const nft = await metaplex.nfts().findByMint(tokenAddress);
-      updateTxId = await this.updateNftService.updateNft(nft.uri, {
-        is_mutable: nft.isMutable,
-        name: nft.name,
+    try {
+      const {
         network,
-        primary_sale_happened: nft.primarySaleHappened,
-        royalty: nft.sellerFeeBasisPoints,
-        symbol: nft.symbol,
-        token_address: nft.mint.toBase58(),
-        update_authority: nft.updateAuthority.toBase58(),
-        new_update_authority: toAddress.toBase58(),
-        private_key: fromAdress,
-      });
+        from_address: fromAdress,
+        transfer_authority,
+      } = transferNftDto;
+      const tokenAddress = new PublicKey(transferNftDto.token_address);
+      const toAddress = new PublicKey(transferNftDto.to_address);
+
+      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+
+      // generate wallet
+      const fromKeypair = AccountUtils.getKeypair(fromAdress);
+      const wallet = new NodeWallet(fromKeypair);
+
+      // Find token accounts
+      const fromAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromKeypair,
+        tokenAddress,
+        fromKeypair.publicKey,
+      );
+      const toAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromKeypair,
+        tokenAddress,
+        toAddress,
+      );
+
+      // Create transfer instruction
+      const ix = createTransferCheckedInstruction(
+        fromAccount.address,
+        tokenAddress,
+        toAccount.address,
+        fromKeypair.publicKey,
+        1,
+        0,
+      );
+      const tx = new Transaction().add(ix);
+      tx.feePayer = fromKeypair.publicKey;
+      tx.recentBlockhash = (
+        await connection.getLatestBlockhash('finalized')
+      ).blockhash;
+
+      // Sign and send it
+      const signedTx = await wallet.signTransaction(tx);
+      const transferTxId = await connection.sendRawTransaction(
+        signedTx.serialize(),
+      );
+
+      const isSuccessful = await connection.confirmTransaction(transferTxId);
+
+      //Now update the authority
+      let updateTxId;
+      if (isSuccessful.value.err === null && transfer_authority) {
+        //Update Authority
+        const metaplex = Metaplex.make(connection);
+        const nft = await metaplex.nfts().findByMint(tokenAddress);
+        updateTxId = await this.updateNftService.updateNft(nft.uri, {
+          is_mutable: nft.isMutable,
+          name: nft.name,
+          network,
+          primary_sale_happened: nft.primarySaleHappened,
+          royalty: nft.sellerFeeBasisPoints,
+          symbol: nft.symbol,
+          token_address: nft.mint.toBase58(),
+          update_authority: nft.updateAuthority.toBase58(),
+          new_update_authority: toAddress.toBase58(),
+          private_key: fromAdress,
+        });
+      }
+      return { updateTxId: updateTxId?.txId ?? 'update_authority not transfered', transferTxId: transferTxId };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.message || error, error.status);
     }
-    return { updateTxId: updateTxId?.txId ?? 'update_authority not transfered', transferTxId: transferTxId };
   }
-  catch(error) {
-    console.log(error);
-    throw new HttpException(error.message || error, error.status);
+
+  async transferNftDetach(transferNftDto: TransferNftDto): Promise<any> {
+    try {
+      const {
+        network,
+        from_address: fromAdress,
+        transfer_authority,
+      } = transferNftDto;
+      const tokenAddressPubKey = new PublicKey(transferNftDto.token_address);
+      const toAddressPubKey = new PublicKey(transferNftDto.to_address);
+
+      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+
+      // generate wallet
+      const fromAddressPubKey = new PublicKey(fromAdress);
+
+      // Find token accounts
+      const fromAccount = await getAssociatedTokenAddress(
+        tokenAddressPubKey,
+        fromAddressPubKey,
+        false,
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+      );
+
+      const toAccount = await getAssociatedTokenAddress(
+        tokenAddressPubKey,
+        toAddressPubKey,
+        false,
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+      );
+
+      let tx: Transaction;
+
+      if (!transfer_authority) {
+        // Create transfer instruction
+        tx = new Transaction().add(
+          createTransferCheckedInstruction(
+            fromAccount,
+            tokenAddressPubKey,
+            toAccount,
+            fromAddressPubKey,
+            1,
+            0,
+          ),
+        );
+      } else {
+        const metaplex = Metaplex.make(connection);
+        const nft = await metaplex.nfts().findByMint(tokenAddressPubKey);
+        const pda = await Metadata.getPDA(tokenAddressPubKey);
+
+        tx = new Transaction().add(
+          createTransferCheckedInstruction(
+            fromAccount,
+            tokenAddressPubKey,
+            toAccount,
+            fromAddressPubKey,
+            1,
+            0,
+          ),
+          createUpdateMetadataAccountV2Instruction(
+            {
+              metadata: pda,
+              updateAuthority: toAddressPubKey,
+            },
+            {
+              updateMetadataAccountArgsV2: {
+                data: {
+                  name: nft.name,
+                  symbol: nft.symbol,
+                  uri: nft.uri,
+                  sellerFeeBasisPoints: nft.sellerFeeBasisPoints,
+                  creators: nft.creators,
+                  collection: nft.collection,
+                  uses: nft.uses,
+                } as DataV2,
+                updateAuthority: toAddressPubKey,
+                primarySaleHappened: nft.primarySaleHappened,
+                isMutable: nft.isMutable,
+              },
+            },
+          ),
+        );
+      }
+
+      tx.feePayer = fromAddressPubKey;
+      tx.recentBlockhash = (
+        await connection.getLatestBlockhash('finalized')
+      ).blockhash;
+
+      const serializedTransaction = tx.serialize({ requireAllSignatures: false, verifySignatures: false });
+      const transactionBase64 = serializedTransaction.toString('base64');
+      return { encoded_transaction: transactionBase64 };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.message || error, error.status);
+    }
   }
 }
