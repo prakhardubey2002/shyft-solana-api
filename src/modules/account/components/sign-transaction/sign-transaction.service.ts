ac7cd97df831d977aa060c2f5f1e79b8b35f49f3
Ishkaran Singh <ishkaran@psychostore.in>
2022-08-11 02:59:23 +0530

    Fix deployment due to shyft/js github (#45)

--- src/modules/account/components/sign-transaction/sign-transaction.service.ts
+++ src/modules/account/components/sign-transaction/sign-transaction.service.ts
@@ -1,5 +1,8 @@
+import { NodeWallet } from '@metaplex/js';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { confirmTransactionFromBackend } from 'shyft-js';
+import { clusterApiUrl, Keypair, Transaction } from '@solana/web3.js';
+import { decode } from 'bs58';
+import { Connection } from '@solana/web3.js';
 
 import { SignTransactionDto } from './dto/sign-transaction.dto';
 
@@ -8,12 +11,18 @@ export class SignTransactionService {
   async signTransaction(signTransactionDto: SignTransactionDto): Promise<any> {
     try {
       const { network, private_key, encoded_transaction } = signTransactionDto;
-      const confirmTransaction = await confirmTransactionFromBackend(
-        network,
-        encoded_transaction,
-        private_key,
+      const connection = new Connection(clusterApiUrl(network), 'confirmed');
+      const feePayer = Keypair.fromSecretKey(decode(private_key));
+      const wallet = new NodeWallet(feePayer);
+      const recoveredTransaction = Transaction.from(Buffer.from(encoded_transaction, 'base64'));
+      const signedTx = await wallet.signTransaction(recoveredTransaction);
+      signedTx.recentBlockhash = (await connection.getLatestBlockhash('finalized')).blockhash;
+      const confirmTransaction = await connection.sendRawTransaction(
+        signedTx.serialize(),
       );
+
       return confirmTransaction;
+
     } catch (error) {
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
