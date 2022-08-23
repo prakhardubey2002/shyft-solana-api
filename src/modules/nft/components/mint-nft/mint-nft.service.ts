18aa5a556fa2c15e621dd47992d86c26d1d1fa14
Pintu Das <rex@workwithme.in>
2022-08-23 14:09:31 +0530

    mint API fixed

--- src/modules/nft/components/mint-nft/mint-nft.service.ts
+++ src/modules/nft/components/mint-nft/mint-nft.service.ts
@@ -9,24 +9,31 @@ import * as BN from 'bn.js';
 @Injectable()
 export class MintNftService {
   async printNewEdition(printNftEditionDto: PrintNftEditionDto): Promise<any> {
-    const {
-      network,
-      private_key,
-      master_nft_address,
-      receiver,
-      transfer_authority,
-    } = printNftEditionDto;
-    const feePayer = AccountUtils.getKeypair(private_key);
-    const connection = new Connection(clusterApiUrl(network));
-    const metaplex = Metaplex.make(connection).use(keypairIdentity(feePayer));
-    const master_address = new PublicKey(master_nft_address);
-    const newOwner = receiver ? new PublicKey(receiver) : feePayer.publicKey;
-    const newUpdateAuthority = transfer_authority ? newOwner : feePayer.publicKey;
-    const { nft: printedNft, transactionId: txId } = await metaplex.nfts().printNewEdition(master_address, {
-      newUpdateAuthority,
-      newOwner,
-    });
-    return { mint: printedNft.mint.toBase58(), txId: txId };
+    try {
+      const {
+        network,
+        private_key,
+        master_nft_address,
+        receiver,
+        transfer_authority,
+      } = printNftEditionDto;
+      const feePayer = AccountUtils.getKeypair(private_key);
+      const connection = new Connection(clusterApiUrl(network));
+      const metaplex = Metaplex.make(connection).use(keypairIdentity(feePayer));
+      const master_address = new PublicKey(master_nft_address);
+      const newOwner = receiver ? new PublicKey(receiver) : feePayer.publicKey;
+      const newUpdateAuthority = transfer_authority ? newOwner : feePayer.publicKey;
+      const { nft: printedNft, transactionId: txId } = await metaplex
+        .nfts()
+        .printNewEdition(master_address, {
+          newUpdateAuthority,
+          newOwner,
+        })
+        .run();
+      return { mint: printedNft.mint?.toBase58(), txId: txId };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
   async printNewEditionDetach(printNftEditionDetachDto: PrintNftEditionDetachDto): Promise<any> {
